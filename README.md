11047f195202390e30bece54f571f544daeae6c0
github@tempert.nu <github@tempert.nu>
2020-03-31 13:32:55 +0200

    feature(docs): Added few lines of comments

---
+++ README.md
@@ -0,0 +1,4 @@
+# Fibonacci
+The Fibonacci sequence in Rust.
+It will calculate 1000 times 100 million numbers of the Fibonacci sequence.
+For the Java version click [here](https://github.com/SemtexError/java-fibonacci).
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,12 @@ fn calc (index: i32)
     let mut rng = rand::thread_rng();
     let random: f64 = rng.gen();
 
+    // Wrapping because Java will do the same when it overflows
     let mut x = Wrapping(0i64);
     let mut y = Wrapping(1i64);
-    let mut z = Wrapping(0i64);
+    let mut z: Wrapping<i64>;
+
+    // Add a random number so it cant be optimize on compile
     let max = 100000000 + (random * 10.0).round() as i32;
 
     for _ in 0..max
